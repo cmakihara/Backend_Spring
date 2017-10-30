ddfe3cb61c375a9792efc5c87d3c2c1245c6be5b
cmakihara <clmakihara@gmail.com>
2017-10-29 22:00:53 -0200

    .

--- agendaapi/src/main/java/br/casa/agendaapi/controller/ContatoController.java
+++ agendaapi/src/main/java/br/casa/agendaapi/controller/ContatoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,10 @@ public class ContatoController {
     public ResponseEntity<Contato> deleteContato(@PathVariable(value = "id") Long contatoId) {
     	contatoRepository.delete(contatoId);
     	return null;
+    }
+    @PutMapping("/agenda/{id}")
+    public Contato putContato(@PathVariable(value = "id") Long contatoId,@Valid @RequestBody Contato contato) {
+        return contatoRepository.save(contato);
     }
 
 
